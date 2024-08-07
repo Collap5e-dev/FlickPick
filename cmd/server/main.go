f5aea83b40f6cb8794e225dfd1137ada5a66c8fb
Coiiap5e <igarkach@bk.ru>
2024-08-07 16:53:51 +0300

    cfg, fmt and for to handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,30 +1,20 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
 
-	"github.com/Collap5e-dev/FlickPick/internal/config"
+	// "github.com/Collap5e-dev/FlickPick/internal/config"
 	"github.com/Collap5e-dev/FlickPick/internal/handler"
 	"github.com/gorilla/mux"
-
 	// "github.com/Collap5e-dev/FlickPick/internal/handler"
-	"time"
+	// "time"
 )
 
 func main() {
-	cfg, err := config.Load()
-	if err != nil {
-		panic(err)
-	}
 	router := mux.NewRouter()
-
 	router.HandleFunc("/", handler.Home)
 	http.Handle("/", router)
 	http.ListenAndServe(":8080", router)
-	fmt.Println(cfg)
-	fmt.Println("Running...")
-	for {
-		time.Sleep(time.Second)
-	}
+
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,8 +3,20 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/Collap5e-dev/FlickPick/internal/config"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome Home!")
+	cfg, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(cfg)
+	fmt.Println("Running...")
+	for {
+		time.Sleep(time.Second)
+	}
 }
